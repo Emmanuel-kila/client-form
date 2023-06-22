7ecca3c7ee1b849dd950c4b6a5edaacd480ea724
Emmanuel Kiptoo <emmanuelkiptoo98@gmail.com>
2023-06-22 13:31:38 +0300

    Changes in the labels and width ratios
    -Changes in the labels and width ratios
    -Added required fields.
    -other minor changes.

---
+++ validation.js
@@ -0,0 +1,15 @@
+//This code is still under development, alot of fields need good validation form
+
+// function validateForm() {
+// 	var x = document.forms["First_Client_Form"]["clientId"].value;
+// 	var y = document.forms["First_Client_Form"]["appId"].value;
+// 	if (x == "") {
+//     	alert("Client Name must be filled");
+//     	return false;
+// 	}
+// 	else if (y.length < 8)
+//    	 {
+//     	alert("Application ID must be at least 8 characters long.");
+//     	return false;
+// }
+// }
